3efbc05cbceb3baa7ec66f3b0ac90240dc1b8fcb
tingyo <tingshuo.yo@googlemail.com>
2018-02-24 01:45:39 +0800

    - Updated run.qpf.r

--- workspace/run.qpf.r
+++ workspace/run.qpf.r
@@ -19,42 +19,6 @@ nstation <- length(ys.tpe2016)
 for(i in 1:nstation){
   # 
   print(paste("Creating IO data for",names(ys.tpe2016)[i]))
-  # Combine IO
-  iodata <- cbind("y"=ys.tpe2016[[i]]$t1hr, input.2016.pc20)
-  # Clean up NA and move date to row.names
-  print("Cleaning up data...")
-  row.names(iodata) <- ys.tpe2016[[i]]$date
-  iodata <- iodata[complete.cases(iodata),]
-  print(paste("    Number of valid records:", nrow(iodata)))
-  # Setup train-control
-  print("Creating folds for cross validation...")
-  set.seed(rseed)
-  cvOut <- createFolds(iodata$y, k=10)
-  cvIn <- cvOut
-  for(i in 1:10){
-    cvIn[[i]] <- (1:length(iodata$y))[-cvOut[[i]]]
-  }
-  trctrl <- trainControl("cv", index=cvIn, indexOut=cvOut, savePred=T)
-  # Fit model
-  print("Training and cross validating...")
-  # GLM
-  fit.glm <- train(y~., data=iodata, method="glm", trControl=trctrl)
-  print("GLM")
-  print(fit.glm$results)
-  #list.glm <- c(list.glm, list(fit.glm))
-  results.glm <- rbind(results.glm, fit.glm$results)
-  coef.glm <- c(coef.glm, list(coef(summary(fit.glm))))
-  # SVM
-  fit.svmr <- train(y~., data=iodata, method="svmRadial", trControl=trctrl)
-  print("SVR")
-  print(fit.svmr$results)
-  #list.svm <- c(list.svm, list(fit.svmr))
-  results.svm <- rbind(results.svm, fit.svmr$results[1,])
-  # Collection predictions
-  y <- iodata$y
-  y.glm <- fit.glm$finalModel$fitted.values
-  y.svm <- fit.svmr$pred$pred
-  ys <- c(ys, list(data.frame("y"=y, "y.glm"=y.glm, "y.svm"=y.svm)))
   # Extract Y
   y <- ys.tpe2016[[i]]
   # Evaluate QPF
