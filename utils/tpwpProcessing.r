d4b985d8d1f9ba7d9bad3692fe9266e15042fbd2
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2018-05-09 15:34:41 +0800

    - Cleaned up old tests - Added functions to utils/tpwpProcessing.r - Test on 2013~2016 data

--- utils/tpwpProcessing.r
+++ utils/tpwpProcessing.r
@@ -15,8 +15,9 @@ read.tpwp <- function(fname, filter.year=NULL){
   names(tmp) <- c("date","t1hr","t3hr","t6hr","t12hr","t24hr","t48hr","t72hr","t120hr")
   # 
   tmp$date <- as.character(tmp$date)
+  print(filter.year)
   if(!is.null(filter.year)){
-    tmp <- tmp[which(substr(tmp$date,1,4)==filter.year),]
+    tmp <- tmp[which(substr(tmp$date,1,4) %in% filter.year),]
   }
   row.names(tmp) <- NULL
   return(tmp)
@@ -32,16 +33,68 @@ create.output <- function(datadir, filter.year=NULL){
   for(f in flist){
     fname <- paste(datadir,f,sep="/")
     print(paste("Processing file:",fname))
-    tmp <- read.tpeqp(fname, filter.year=filter.year)
+    tmp <- read.tpwp(fname, filter.year=filter.year)
+    # Check results
+    expected.size <- c(365*24*length(filter.year), 365*24*length(filter.year)+24)
     print(dim(tmp))
-    if(nrow(tmp) %in% c(365*24, 366*24) ){
+    if(nrow(tmp) %in% expected.size){
       outputs <- c(outputs, list(tmp))
       onames <- c(onames, f)
     } else {
-      print(paste("Number of records incorrect:", nrow(tmp)))
+      print(paste("Number of records incorrect:", nrow(tmp), "expected:", expected.size[1]))
     }
   }
   names(outputs) <- substr(onames,1,6)
   return(outputs)
 }
 
+# Create input data for QPE:
+# - Use given Y(precipitation) and it's time stamp(YYYYMMDDHH)
+# - For the same YYYYMMDD, each HH of precipitation is corresponding to: 
+#   - 6 HHMM of DBZ data records (with 10min interval)
+#   - HH-60min, HH-50min, HH-40min, HH-30min, HH-20min, and HH-10min
+#   - impute NA for missing DBZ records
+create.input.qpe <- function(y, x, filter.year=NULL){
+  # Table for hour-minute correspondence
+  hhlist <- c("00","01","02","03","04","05","06","07","08","09","10","11",
+              "12","13","14","15","16","17","18","19","20","21","22","23")
+  mmlist <- c("00","10","20","30","40","50")
+  hhmm <- NULL
+  for(h in hhlist){hhmm <- c(hhmm, paste0(h, mmlist))}
+  hhmm <- matrix(hhmm, ncol=6, byrow=T)
+  # Target hour
+  thh <- c("01","02","03","04","05","06","07","08","09","10","11","12",
+           "13","14","15","16","17","18","19","20","21","22","23","24")
+  # target day-hour
+  ydays <- sort(unique(substr(y$date,1,8)))
+  newx <- NULL
+  for(i in 1:nrow(y)){
+    # Process date-hour string
+    ydh <- y$date[i]                # YYYYMMDDHH string
+    yday <- substr(ydh,1,8)         # YYYYMMDD
+    yhh <- substr(ydh,9,10)         # HH
+    idx.day <- which(ydays==yday)   # Index of YYYYMMDD
+    idx.h <- which(thh==yhh)        # Index of HH
+    # Find corresponding X
+    ridx.x <- which((x[,1]==yday) & (x[,2] %in% hhmm[idx.h,]))
+    # Concatenate all x as the new x
+    print(ydh)
+    #print(length(ridx.x))
+    #print(x[ridx.x, 1:2])
+    if(length(ridx.x)==6){
+      rec <- as.vector(t(x[ridx.x, -(1:2)]))
+      #print(length(rec))
+    } else {
+      print("missing data")
+      rec <- rep(NA, 6*(ncol(x)-2))
+    }
+    newx <- rbind(newx, rec)
+  }
+  # Create colnames
+  newx <- data.frame(newx)
+  pcn <- names(x)[-(1:2)]
+  ts <- paste0("m", seq(60,10,-10))
+  names(newx) <- apply(expand.grid(pcn, ts), 1, paste, collapse="_")
+  #
+  return(cbind(y,newx))
+}
--- workspace/run.qpe.r
+++ workspace/run.qpe.r
@@ -2,7 +2,7 @@
 # Description:
 #    Functions to perfrom QPE from radar data
 # Load input/output data
-load("io.tpe2016.pc20.RData")
+#load("io.tpe2016.pc20.RData")
 rseed = 1234543
 # Load library
 require(caret)
@@ -14,15 +14,15 @@ results.glm <- data.frame(NULL)
 #coef.glm <- NULL
 #ys <- NULL
 # Run through each station
-nstation <- length(ys.tpe2016)
+nstation <- length(y.1316)
 for(i in 1:nstation){
   # 
-  print(paste("Creating IO data for",names(ys.tpe2016)[i]))
+  print(paste("Creating IO data for",names(y.1316)[i]))
   # Combine IO
-  iodata <- cbind("y"=ys.tpe2016[[i]]$t1hr, input.2016.pc20)
+  iodata <- cbind("y"=y.1316[[i]]$t1hr, input.1316)
   # Clean up NA and move date to row.names
   print("Cleaning up data...")
-  row.names(iodata) <- ys.tpe2016[[i]]$date
+  row.names(iodata) <- y.1316[[i]]$date
   iodata <- iodata[complete.cases(iodata),]
   print(paste("    Number of valid records:", nrow(iodata)))
   # Setup train-control
@@ -60,14 +60,14 @@ for(i in 1:nstation){
   #y$y.svm <- fit.svmr$pred$pred
   #ys <- c(ys, list(y))
 }
-#names(list.glm) <- names(ys.tpe2016)
-#names(list.svm) <- names(ys.tpe2016)
-#names(coef.glm) <- names(ys.tpe2016)
-#names(ys) <- names(ys.tpe2016)
+#names(list.glm) <- names(y.1316)
+#names(list.svm) <- names(y.1316)
+#names(coef.glm) <- names(y.1316)
+#names(ys) <- names(y.1316)
 # Clean up
 rm(i, iodata, cvOut, cvIn, trctrl, fit.glm, fit.svmr)
 # Save
-save.image("qpe2016.RData")
+save.image("qpe1316.RData")
 
 
 
