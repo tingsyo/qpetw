c66889c1acebdd750c175c21fee0a0efac40de64
Ting <tingshuo.yo@googlemail.com>
2018-10-08 18:36:46 +0800

    Work in progress: lib/dbzCNN.py Deleted unused files

--- utils/dbzCNN.py
+++ utils/dbzCNN.py
@@ -39,6 +39,13 @@ __email__ = "tingyo@dataqualia.com"
 __status__ = "development"
 __date__ = '2018-02-14'
 
+# Parameters
+nChannel = 6                    # 6 10-min dbz for an hour
+nX = 275                        # x-dimension of dbz data
+nY = 162                        # y-dimension of dbz data
+nSample = 37369                 # Total complete samples
+yseg_stat = [0.5, 8, 13, 29]    # 40-year statistics of hourly precipitation of trace, 90%, 95%, and 99% percentile
+yseg = [0.5, 10, 15, 30]        # Actual segmentation for precipitation
 
 # VGG model
 def init_model_vgg(input_shape):
@@ -48,48 +55,49 @@ def init_model_vgg(input_shape):
     :param 
       int input_shape: The number of variables to use as input features.
     """
-    # Initialize model
-    model = Sequential()
+    # Input layer
+    inputs = Input(shape=input_shape)
     # blovk1: CONV -> CONV -> MaxPooling
-    model.add(Conv2D(filters=64, kernel_size=(3,1), strides=(1,1), activation='relu', name='block1_conv1', kernel_initializer=initializers.glorot_normal(), input_shape=input_shape))
-    model.add(Conv2D(64, (3,1), activation='relu', name='block1_conv2', kernel_initializer=initializers.glorot_normal()))
-    model.add(MaxPooling2D((2,1), strides=(2,1), name='block1_pool'))
-    model.add(Dropout(0.2))
+    x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu', name='block1_conv1', kernel_initializer=initializers.glorot_normal())(inputs)
+    x = Conv2D(64, (3,3), activation='relu', name='block1_conv2', kernel_initializer=initializers.glorot_normal())(x)
+    x = MaxPooling2D((2,2), strides=(1,1), name='block1_pool')(x)
+    x = Dropout(0.5)(x)
     # block2: CONV -> CONV -> MaxPooling
-    model.add(Conv2D(128, (3,1), activation='relu', name='block2_conv1', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(128, (3,1), activation='relu', name='block2_conv2', kernel_initializer=initializers.glorot_normal()))
-    model.add(MaxPooling2D((2,1), strides=(2,1), name='block2_pool'))
-    model.add(Dropout(0.2))
+    x = Conv2D(128, (3,3), activation='relu', name='block2_conv1', kernel_initializer=initializers.glorot_normal())(x)
+    x = Conv2D(128, (3,3), activation='relu', name='block2_conv2', kernel_initializer=initializers.glorot_normal())(x)
+    x = MaxPooling2D((2,2), strides=(1,1), name='block2_pool')(x)
+    x = Dropout(0.5)(x)
     # block3: CONV -> CONV -> MaxPooling
-    model.add(Conv2D(256, (3,1), activation='relu', name='block3_conv1', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(256, (3,1), activation='relu', name='block3_conv2', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(256, (3,1), activation='relu', name='block3_conv3', kernel_initializer=initializers.glorot_normal()))
-    model.add(MaxPooling2D((2,1), strides=(2,1), name='block3_pool'))
-    model.add(Dropout(0.2))
-    # block4: CONV -> CONV -> MaxPooling
-    model.add(Conv2D(512, (3,1), activation='relu', name='block4_conv1', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(512, (3,1), activation='relu', name='block4_conv2', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(512, (3,1), activation='relu', name='block4_conv3', kernel_initializer=initializers.glorot_normal()))
-    model.add(MaxPooling2D((2,1), strides=(2,2), name='block4_pool'))
-    model.add(Dropout(0.2))
-    # block5: CONV -> CONV -> MaxPooling -> Dropout
-    model.add(Conv2D(512, (3,1), activation='relu', name='block5_conv1', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(512, (3,1), activation='relu', name='block5_conv2', kernel_initializer=initializers.glorot_normal()))
-    model.add(Conv2D(512, (3,1), activation='relu', name='block5_conv3', kernel_initializer=initializers.glorot_normal()))
-    model.add(MaxPooling2D((2,1), strides=(2,2), name='block5_pool'))
-    model.add(Dropout(0.2))
-    # Output block: Flatten -> Dense -> Dense -> regression output
-    model.add(Flatten())  
-    model.add(Dense(512, activation='relu', name='fc1'))
-    model.add(Dropout(0.8))
-    model.add(Dense(512, activation='relu', name='fc2'))
-    model.add(Dense(1, activation='linear', name='prediction')) 
+    x = Conv2D(256, (3,3), activation='relu', name='block3_conv1', kernel_initializer=initializers.glorot_normal())(x)
+    x = Conv2D(256, (3,3), activation='relu', name='block3_conv2', kernel_initializer=initializers.glorot_normal())(x)
+    x = Conv2D(256, (3,3), activation='relu', name='block3_conv3', kernel_initializer=initializers.glorot_normal())(x)
+    x = MaxPooling2D((2,2), strides=(1,1), name='block3_pool')(x)
+    x = Dropout(0.5)(x)
+    # Output block: Flatten -> Dense -> Dense -> softmax output
+    x = Flatten()(x)
+    x = Dense(512, activation='relu', name='fc1')(x)
+    x = Dropout(0.8)(x)
+    x = Dense(512, activation='relu', name='fc2')(x)
+    # Output layer
+    out = Dense(5, activation='sigmoid', name='main_output')(x)
+    # Initialize model
+    model = Model(inputs = inputs, outputs = out)
     # Define compile parameters
-    adam = Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.01, clipvalue=1.)
-    model.compile(loss='cosine_proximity', optimizer=adam, metrics=['mse','mae'])
+    #adam = Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.01, clipvalue=1.)
+    sgd = SGD(lr=0.1, momentum=1e-8, decay=0.01, nesterov=True, clipvalue=1.)
+    model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return(model)
 
-def read_dbz(furi):
+def read_x(furi):
+    results = None
+    try:
+        tmp = pd.read_fwf(furi, widths=[8,8,8], header=None)
+        results = np.float32(np.array(tmp.iloc[:,2])).reshape((275,162))
+    except pd.errors.EmptyDataError:
+        logging.warning(furi + " is empty.")
+    return(results)
+
+def read_y(furi):
     results = None
     try:
         tmp = pd.read_fwf(furi, widths=[8,8,8], header=None)
@@ -104,37 +112,26 @@ def main():
     parser = argparse.ArgumentParser(description='Retrieve DBZ data for further processing.')
     parser.add_argument('--input', '-i', help='the directory containing all the DBZ data.')
     parser.add_argument('--output', '-o', default='output.csv', help='the output file.')
-    parser.add_argument('--batch_size', '-b', default=100, type=int, help='number of component to output.')
+    parser.add_argument('--batch_size', '-b', default=100, type=int, help='batch size.')
     parser.add_argument('--randomseed', '-r', help="integer as the random seed", default="1234543")
     parser.add_argument('--log', '-l', default='tmp.log', help='the log file.')
     args = parser.parse_args()
     # Set up logging
     logging.basicConfig(filename=args.log, filemode='w', level=logging.DEBUG)
-    # Scan files for reading
-    finfo = search_dbz(args.input)
-    # Read into numpy.memmap
+    # Read input and output
     logging.info("Extract data from all files: "+ str(len(finfo)))
-    dbz = read_dbz_memmap(finfo)
-    # Process dbz data with Incremental PCA
+
+    logging.info("Extract data from all files: "+ str(len(finfo)))
+    
+    # Set up cross validation
+    logging.info("Extract data from all files: "+ str(len(finfo)))
+    
+    # Train and Test
     logging.info("Performing IncrementalPCA with "+ str(args.n_components)+" components.")
-    ipca = IncrementalPCA(n_components=args.n_components, batch_size=args.batch_size)
-    dbz_ipca = ipca.fit_transform(dbz)
-    evr = ipca.explained_variance_ratio_
-    com = np.transpose(ipca.components_)
+    
     logging.debug("Explained variance ratio: "+ str(evr))
-    # Output components
-    com_header = ['pc'+str(x+1) for x in range(args.n_components)]
-    #np.insert(com, 0, com_header, axis=0)
-    writeToCsv(com, args.output.replace('.csv','.components.csv'), header=com_header)
-    # Append date and projections
-    proj_header = ['date','hhmm'] + ['pc'+str(x+1) for x in range(args.n_components)]
-    newrecs = []
-    for i in range(len(finfo)):
-        newrecs.append(finfo[i][1:3] + list(dbz_ipca[i]))
-    # Output
-    writeToCsv(newrecs, args.output, header=proj_header)
-    # Save PCA for later use
-    joblib.dump(ipca, args.output.replace(".csv",".pca.mod"))
+    # Output results
+    
     # done
     return(0)
     
