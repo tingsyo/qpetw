36c7a4df2ea694556281a18fd92fcd4cec1c8fca
Ting <tingshuo.yo@googlemail.com>
2018-10-11 15:36:52 +0800

    Work in progress: dbzCNN.py - Added data loading function. - Added cross-validation function - ToDo: train and test

--- utils/dbzCNN.py
+++ utils/dbzCNN.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import random as rn
+# For cross-validation
+from sklearn.model_selection import StratifiedKFold
 # For fixing random state: block1
 os.environ['PYTHONHASHSEED'] = '0'
 np.random.seed(14221)
@@ -40,12 +42,73 @@ __status__ = "development"
 __date__ = '2018-02-14'
 
 # Parameters
-nChannel = 6                    # 6 10-min dbz for an hour
-nX = 275                        # x-dimension of dbz data
-nY = 162                        # y-dimension of dbz data
 nSample = 37369                 # Total complete samples
+nLayer = 6                      # 6 10-min dbz for an hour
+nY = 275                        # y-dimension of dbz data
+nX = 162                        # x-dimension of dbz data
+batchSize = 128                 # Batch size for training / testing
 yseg_stat = [0.5, 8, 13, 29]    # 40-year statistics of hourly precipitation of trace, 90%, 95%, and 99% percentile
 yseg = [0.5, 10, 15, 30]        # Actual segmentation for precipitation
+
+#-----------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------
+def createIOTable(x, y, ylab='t1hr', qpf=False):
+    ''' 
+    Given the input/output data, check for data completeness and match records by date.
+    - Start with ydate because the precipitation date is always complete.
+    - Loop through x, associate x with y of the same date.
+    - Check the flag QPF. If QPF, shift the x(t)-y(t) mapping to y(t)-x(t-1).
+    - Check through x-y list, keep only complete cases.
+    - Return input/output.
+    '''
+    import pandas as pd
+    # Clean up dates of x and y
+    xdate = [f.split('.')[0].split('/')[-1] for f in x]
+    ydate = np.array(y['date'], dtype='str')
+    xfull = zip(xdate, x)                                   # Pair up date of x and the uri
+    x_in_y = [x for x in xfull if x[0] in ydate]            # Valid xdate: xdate existing in ydate
+    xd, xuri = zip(*x_in_y)
+    xidx = [list(ydate).index(x) for x in xd]               # Index of valid xdate in ydate
+    # Check qpf flag, if true use earlier xdate
+    if(qpf):
+        xidx = [x+1 for x in xidx if(x+1)<len(ydate)]
+        x_in_y = x_in_y[:len(xidx)]
+    # Match days
+    md = pd.DataFrame({'date':ydate, 'y':np.array(y[ylab], dtype=np.float32), 'x':None})
+    md = md.iloc[xidx,:]
+    md['x'] = xuri
+    # Scan for complete x-y records
+    cd = []
+    for i in range(md.shape[0]):
+        if not (None in list(md.iloc[i,:])):
+            cd.append(list(md.iloc[i,:]))
+            #print(list(md.iloc[i,:]))
+    # Done
+    return(pd.DataFrame(cd, columns=['date','y','xuri']))
+
+# Load input/output data for model
+def loadIOData(srcx, srcy):
+    # Read raw input and output
+    logging.info("Reading input X from: "+ srcx)
+    xfiles = glob.glob(srcx+'/*.npy')
+    logging.info("Reading output Y from: "+ srcy)
+    yraw = pd.read_csv(srcy)
+    # Create complete IO-data
+    iotab = createIOTable(xfiles, yraw)   
+    nSample = len(iotab)
+    # Create paired Input data
+    fx = h5py.File(args, 'w')
+    xdata = fx.create_dataset('x', (nSample, nLayer, nY, nX), chunks=(batchSize, nLayer, nY, nX), dtype=np.float32)
+    for i in range(nSample):
+        tmp = np.load(iotab['xuri'][i])
+        xdata[i] = tmp
+    # Create categorized output data
+    y = np.array(iotab['y'], dtype=np.float32).reshape(nSample, 1)
+    ydata = np.digitize(y, yseg)
+    #ydata = to_categorical(ycat)
+    # Done
+    return(ydata, xdata)
 
 # VGG model
 def init_model_vgg(input_shape):
@@ -83,7 +146,6 @@ def init_model_vgg(input_shape):
     # Initialize model
     model = Model(inputs = inputs, outputs = out)
     # Define compile parameters
-    #adam = Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.01, clipvalue=1.)
     sgd = SGD(lr=0.1, momentum=1e-8, decay=0.01, nesterov=True, clipvalue=1.)
     model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return(model)
@@ -110,27 +172,37 @@ def read_y(furi):
 def main():
     # Configure Argument Parser
     parser = argparse.ArgumentParser(description='Retrieve DBZ data for further processing.')
-    parser.add_argument('--inputx', '-x', help='the directory containing all the DBZ data.')
-    parser.add_argument('--inputy', '-y', help='the file containing precipitation data.')
-    parser.add_argument('--output', '-o', default='output.csv', help='the output file.')
-    parser.add_argument('--batch_size', '-b', default=100, type=int, help='batch size.')
+    parser.add_argument('--rawx', '-x', help='the directory containing preprocessed DBZ data.')
+    parser.add_argument('--rawy', '-y', help='the file containing the precipitation data.')
+    parser.add_argument('--input', '-i', default='x.hdf5', help='the processed input data.')
+    parser.add_argument('--output', '-o', default='y.hdf5', help='the processed output data.')
+    parser.add_argument('--kfold', '-k', default=5, type=int, help='K-fold cross validation.')
+    parser.add_argument('--batch_size', '-b', default=128, type=int, help='batch size.')
     parser.add_argument('--log', '-l', default='tmp.log', help='the log file.')
     args = parser.parse_args()
     # Set up logging
     logging.basicConfig(filename=args.log, filemode='w', level=logging.DEBUG)
-    # Read input and output
-    logging.info("Reading input X from: "+ args.inputx)
-    xinfo = pd.read_csv(args.inputx)
-    logging.info("Reading output Y from: "+ args.inputy)
-    yraw = pd.read_csv(args.inputy)
+    #-------------------------------
+    # IO data generation
+    #-------------------------------
+    y, x = loadIOData(args.inputx, args.inputy)
+    #-------------------------------
+    # Cross validation
+    #-------------------------------
     # Set up cross validation
-    logging.info("Extract data from all files: "+ str(len(finfo)))
+    logging.info("Generate data splits for "+ str(args.kfold)+"-fold cross validation.")
+    skf = StratifiedKFold(n_splits=args.kfold)
+    for trainIdx, testIdx in skf.split(x, y):
+        xTrain, xTest = x[trainIdx], x[testIdx]
+        yTrain, yTest = to_categorical(y[trainIdx]), to_categorical(y[testIdx])
+        # Train and 
+        logging.info("Training model with " + str(len(trainIdx)) + " samples.")
+        
+        # Test
+        logging.info("Testing model with " + str(len(testIdx)) + " samples.")
     
-    # Train and Test
-    logging.info("Performing IncrementalPCA with "+ str(args.n_components)+" components.")
-    
-    logging.debug("Explained variance ratio: "+ str(evr))
-    # Output results
+        # Output results
+        logging.debug("Explained variance ratio: "+ str(evr))
     
     # done
     return(0)
