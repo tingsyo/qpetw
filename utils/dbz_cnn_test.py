ae5f51276d789894074919eaaeb138c4ebc7c1a9
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2018-10-13 11:58:12 +0800

    Fixed bugs

--- utils/dbz_cnn_test.py
+++ utils/dbz_cnn_test.py
@@ -75,12 +75,13 @@ def createIOTable(x, y, ylab='t1hr', qpf=False):
         xidx = [x+1 for x in xidx if(x+1)<len(ydate)]
         x_in_y = x_in_y[:len(xidx)]
     # Match days
-    md = pd.DataFrame({'date':ydate, 'y':np.array(y[ylab], dtype=np.float32), 'x':None})
+    md = pd.DataFrame({'date':ydate, 'y':np.array(y[ylab], dtype=np.float32), 'xuri':None})
     md = md.iloc[xidx,:]
+    md['xuri'] = xuri
     # Scan for complete x-y records
     cd = md.loc[~np.isnan(md['y']),:]
     # Done
-    return(cd.reset_index())
+    return(cd)
 
 # Load input/output data for model
 def loadIOTab(srcx, srcy, test_split=0.0, shuffle=False):
@@ -118,9 +119,10 @@ def init_model(input_shape):
     # Input layer
     inputs = Input(shape=input_shape)
     # blovk1: CONV -> CONV -> MaxPooling
-    x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu', name='block1_conv1', data_format='channels_first', kernel_initializer=initializers.glorot_normal())(inputs)
-    x = Conv2D(64, (3,3), activation='relu', name='block1_conv2', data_format='channels_first',kernel_initializer=initializers.glorot_normal())(x)
-    x = MaxPooling2D((2,2), strides=(1,1), name='block1_pool')(x)
+    x = Conv2D(filters=64, kernel_size=(4,4), strides=(2,2), activation='relu', name='block1_conv1', data_format='channels_first', kernel_initializer=initializers.glorot_normal())(inputs)
+    x = Conv2D(64, (4,4), activation='relu', name='block1_conv2', data_format='channels_first',kernel_initializer=initializers.glorot_normal())(x)
+    x = Conv2D(64, (4,4), activation='relu', name='block1_conv3', data_format='channels_first',kernel_initializer=initializers.glorot_normal())(x)
+    x = MaxPooling2D((2,2), strides=(2,2), name='block1_pool')(x)
     x = Dropout(0.5)(x)
     # block2: CONV -> CONV -> MaxPooling
 #    x = Conv2D(128, (3,3), activation='relu', name='block2_conv1',data_format='channels_first', kernel_initializer=initializers.glorot_normal())(x)
@@ -158,6 +160,7 @@ def data_generator(iotab, batch_size):
             limit = min(batch_end, nSample)
             X = loadDBZ(iotab['xuri'][batch_start:limit])
             Y = to_categorical(np.digitize(y[batch_start:limit], yseg))
+            print(X.shape)
             yield (X,Y) #a tuple with two numpy arrays with batch_size samples     
             batch_start += batch_size   
             batch_end += batch_size
@@ -170,7 +173,7 @@ def main():
     parser.add_argument('--rawx', '-x', help='the directory containing preprocessed DBZ data.')
     parser.add_argument('--rawy', '-y', help='the file containing the precipitation data.')
     parser.add_argument('--output', '-o', help='the file to store training history.')
-    parser.add_argument('--kfold', '-k', default=3, type=int, help='K-fold cross validation.')
+    parser.add_argument('--split', '-s', default=0.2, type=np.float32, help='testing split ratio.')
     parser.add_argument('--batch_size', '-b', default=128, type=int, help='batch size.')
     parser.add_argument('--log', '-l', default='tmp.log', help='the log file.')
     args = parser.parse_args()
@@ -179,28 +182,34 @@ def main():
     #-------------------------------
     # IO data generation
     #-------------------------------
-    iotab = loadIOTab(args.rawx, args.rawy, test_split=0.1, shuffle=True)
-    print("Training data dimension:")
-    print(iotab['train'].shape)
-    steps_train = int(len(iotab['train'])/args.batch_size)
-    print("Testing data dimension:")
-    print(iotab['test'].shape)
-    steps_test = int(len(iotab['test'])/args.batch_size)
+    iotab = loadIOTab(args.rawx, args.rawy, test_split=args.split, shuffle=True)
     #-------------------------------
     # Test dnn
     #-------------------------------
     # Train
     #logging.info("Training model with " + str(len(x)) + " samples.")
     model = init_model((nLayer, nY, nX))
+    # Debug info
     print(model.summary())
+    print("Training data samples: "+str(iotab['train'].shape[0]))
+    steps_train = int(len(iotab['train'])/args.batch_size)
+    print("Training data steps: " + str(steps_train))
+    print(iotab['train'][:5])
+    print("Testing data samples: "+ str(iotab['test'].shape[0]))
+    steps_test = int(len(iotab['test'])/args.batch_size)
+    print("Testing data steps: " + str(steps_test))
+    print(iotab['test'][:5])
+    # Fitting model
     hist = model.fit_generator(data_generator(iotab['train'], args.batch_size), steps_per_epoch=steps_train,
            epochs=1, use_multiprocessing=True, verbose=1)
+    # Prediction
     y_pred = model.predict_generator(data_generator(iotab['test'], args.batch_size), steps=steps_test,
              use_multiprocessing=True, verbose=1)
-    y_com = {'y_true':iotab['test']['y'], 'y_pred':y_pred}
+    # Prepare output
+    y_com = {'y_true':iotab['test']['y'], 'y_pred':[np.argmax(y) for y in y_pred]}
     # Output results
     with open(args.output, 'wb') as fout:
-        pickle.dump({'history':hist.history, 'validation':y_com}, fout)
+        pickle.dump({'iotable': iotab, 'model':model.summary(), 'history':hist.history, 'validation':y_com}, fout)
     # done
     return(0)
     
