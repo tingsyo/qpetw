620c45f6b19107e95fce8ccd052b425607e33a76
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2019-07-04 12:38:31 +0800

    completed test script

--- ws.ae/ae_svm_qpe.run.r
+++ ws.ae/ae_svm_qpe.run.r
@@ -66,9 +66,7 @@ library(doParallel)
 cluster <- makeCluster(detectCores() - 2) # convention to leave 1 core for OS
 registerDoParallel(cluster)
 # Collect results
-results.glm <- data.frame(NULL)
-results.svm <- data.frame(NULL)
-ys <- NULL
+cmsByModels <- NULL
 mod.glm <- NULL
 mod.svm <- NULL
 # Run through each station
@@ -99,47 +97,27 @@ for(i in 1:nstation){
   print("Training and cross validating...")
   # GLM
   fit.glm <- train(y~., data=iodata, method="glm", family="binomial", preProcess="scale", trControl=trctrl)
-  rec <- fit.glm$results
-  yhat.glm <- fit.glm$finalModel$fitted.values
-  rec$RMSE.insample <- RMSE(exp(yhat.glm)-1, iodata$y)
-  rec$CORR.log <- cor(yhat.glm, log(iodata$y+1))
-  rec$CORR.mm <- cor(exp(yhat.glm)-1, iodata$y)
-  print("GLM")
-  print(rec)
-  results.glm <- rbind(results.glm, rec)
-  #coef.glm <- c(coef.glm, list(coef(summary(fit.glm))))
   # SVM
-  fit.svmr <- train(y~., data=iodata, method="svmRadial", preProcess="scale", trControl=trctrl)
-  rec <- fit.svmr$results[1,]
-  yhat.svm <- fit.svmr$finalModel@fitted
-  rec$RMSE.insample <- RMSE(exp(yhat.svm)-1, iodata$y)
-  rec$CORR.log <- cor(yhat.svm, log(iodata$y+1))
-  rec$CORR.mm <- cor(exp(yhat.svm)-1, iodata$y)
-  print("SVR")
-  print(rec)
-  results.svm <- rbind(results.svm, rec)
+  fit.svm <- train(y~., data=iodata, method="svmRadial", preProcess="scale", trControl=trctrl)
   # Collection predictions
-  #y <- data.frame(NULL)
-  y <- data.frame(iodata$y)
-  y$y.glm <- yhat.glm
-  y$y.svm <- yhat.svm
-  ys <- c(ys, list(y))
+  cms <- list("glm"=confusionMatrix.train(fit.glm)$table,
+              "svm"=confusionMatrix.train(fit.svm)$table)
   # Save model
   mod.glm <- c(mod.glm, list(fit.glm$finalModel))
   mod.svm <- c(mod.svm, list(fit.svmr$finalModel))
+  cmsByModels <- c(cmsByModels, list(cms))
 }
-#names(coef.glm) <- names(y.1316)
-names(ys) <- names(y.1316)
+names(cmsByModels) <- names(y.1316)
 names(mod.glm) <- names(y.1316)
 names(mod.svm) <- names(y.1316)
 # Clean up
-rm(i, iodata, cvOut, cvIn, trctrl, fit.glm, fit.svmr)
+rm(i, iodata, cvOut, cvIn, trctrl, fit.glm, fit.svm)
 # Stop parallel
 stopCluster(cluster)
 registerDoSEQ()
 # Save
-save(results.glm, results.svm, ys, file="qpe1316.ae.RData")
-#save(mod.glm, mod.svm, ys, file="qpe1316.mod.RData")
+save(results.glm, results.svm, ys, file="qpe1316_ae_svm.results.RData")
+save(mod.glm, mod.svm, ys, file="qpe1316_ae_svm.mod.RData")
 
 
 
