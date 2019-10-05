367214efca42a2161c9949a845be6003f898d720
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2019-10-05 08:24:32 +0800

    Added transform script

---
+++ ws.flt/run_transform.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_full -m ln_fp40.pca.joblib -l ln_fp40.log
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_fp40 -m ln_fp40.pca.joblib -l ln_fp40.log
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_fp20 -m ln_fp20.pca.joblib -l ln_fp20.log
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_fp10 -m ln_fp10.pca.joblib -l ln_fp10.log
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_fp05 -m ln_fp05.pca.joblib -l ln_fp05.log
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_fp01 -m ln_fp01.pca.joblib -l ln_fp01.log
+python ../utils/dbz_ipca_transform.py -i ../data/qpesums_npy/ -o proj_ln_ftyw -m ln_ftyw.pca.joblib -l ln_ftyw.log
